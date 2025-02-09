786d947cd14d341f8545c204255bfdf9f28bc727
Stefano <stefanoedwards@icloud.com>
2025-02-08 22:57:54 -0500

    Update content.js

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -89,7 +89,7 @@ const about = {
   },
   work: {
     display: false, // set to false to hide this section
-    title: "Work Experience",
+    title: "Experience",
     experiences: [
       {
         company: "FLY",
